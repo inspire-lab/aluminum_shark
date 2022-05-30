b6201c8001c4af8568db6241615be46ac54d43bb
Robert Podschwadt <robertpodschwadt@gmail.com>
2022-05-30 12:24:32 -0600

    tests for a simple neural network

--- python/aluminum_shark/test_nn.py
+++ python/aluminum_shark/test_nn.py
@@ -1,7 +1,8 @@
 import os
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
 # os.environ['TF_CPP_MAX_VLOG_LEVEL'] = '1'
-os.environ['ALUMINUM_SHARK_LOGGING'] = '1'
+os.environ['ALUMINUM_SHARK_LOGGING'] = '0'
+os.environ['ALUMINUM_SHARK_BACKEND_LOGGING'] = '0'
 os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
 # os.environ['TF_XLA_FLAGS'] += ' --tf_mlir_enable_mlir_bridge'
 os.environ['TF_DUMP_GRAPH_PREFIX'] = \
@@ -25,6 +26,7 @@ def create_model():
           input_shape=x_in.shape[1:]))
 
   w, b = model.layers[0].get_weights()
+  # print(w.shape)
   model.layers[0].set_weights(
       [np.arange(w.size).reshape(w.shape),
        np.arange(b.size).reshape(b.shape)])
@@ -33,15 +35,6 @@ def create_model():
 
 y_true = create_model()(x_in)
 print(y_true)
-
-# @tf.function(jit_compile=True)
-# def f(x):
-#   # return tf.square(x) + [5, 6, 7, 8]
-#   return tf.square(x) + [[5, 6], [7, 8]]
-
-# # input values
-# # x_in = np.array([1, 2, 3, 4])
-# x_in = np.array([[1, 2], [3, 4]])
 
 # set it all up
 backend = shark.HEBackend(
@@ -53,14 +46,14 @@ ctxt = context.encrypt(x_in, name='x', dtype=float)
 
 # run computation
 enc_model = shark.EncryptedExecution(model_fn=create_model, context=context)
-result_ctxt = enc_model(ctxt)
+result_ctxt = enc_model(ctxt, debug_inputs=[x_in])
 
 # decrypt
-decrypted = context.decrypt_double(result_ctxt)[:4]
+decrypted = context.decrypt_double(result_ctxt[0])
 print('decrypted values', decrypted)
 print('actual values', y_true)
 decrypted = np.array(decrypted)
-if all((decrypted - y_true) < 0.001):
+if np.all(abs(decrypted - y_true) < 0.001):
   print("decryption with in rounding tolerance")
 else:
   raise Exception('decryption does not match plaintext execution')
