b8f9f0eeafafcc8bc712242ebc3c208892bbc4c6
Robert Podschwadt <robertpodschwadt@gmail.com>
2023-01-13 17:36:36 -0700

    added dynamic arguements for context creation

--- python/aluminum_shark/core.py
+++ python/aluminum_shark/core.py
@@ -43,6 +43,118 @@ AS_LOG("Wrapped TensorFlow library: " + tf_lib_path)
 # backend functions        #
 ############################
 
+# // struct to transport data between python and c++.
+# struct Argument {
+#   const char* name;
+#   // 0: int
+#   // 1: double
+#   // 3: string
+#   uint type;
+
+#   // if true the `array_` member will point to an array containing `size_`
+#   // elements of `type`.
+#   bool is_array = false;
+
+#   // data holding variables
+#   long int_;
+#   double double_;
+#   const char* string_;
+#   // holds data if `is_array` == ture.
+#   void* array_ = nullptr;
+#   size_t size_;
+# };
+
+
+def get_argument_type(obj):
+  if isinstance(obj, (int, np.integer)):
+    return 0
+  elif isinstance(obj, (float, np.floating)):
+    return 1
+  elif isinstance(obj, str):
+    return 2
+
+
+class aluminum_shark_Argument(ctypes.Structure):
+  _fields_ = [('name', ctypes.c_char_p), ('type', ctypes.c_uint),
+              ('is_array', ctypes.c_bool), ('int_', ctypes.c_long),
+              ('double_', ctypes.c_double), ('string_', ctypes.c_char_p),
+              ('array_', ctypes.c_void_p), ('size_', ctypes.c_size_t)]
+
+  def __init__(self, name, value) -> None:
+    # default values
+    name_ = name.encode('utf-8')
+    is_array = False
+    int_ = ctypes.c_long()
+    double_ = ctypes.c_double()
+    string_ = ctypes.c_char_p()
+    array_ = ctypes.c_void_p()
+    size_ = ctypes.c_size_t()
+
+    # check if we are dealing with a "basic type"
+    type_ = get_argument_type(value)
+
+    # single value
+    if type_ is not None:
+      # int
+      if type_ == 0:
+        int_ = ctypes.c_long(int(value))
+      # float
+      elif type_ == 1:
+        float_ = ctypes.c_double(float(value))
+      # string
+      elif type_ == 2:
+        string_ = value.encode('utf-8')
+
+    # possibly an array or something we can't handle
+    else:
+      # make sure that we deal with a list that has all the same type
+      types = [get_argument_type(x) for x in value]
+      # check that they are all the same
+      if len(set(types)) > 1:
+        raise ValueError("all list elments must have the same type")
+      type_ = types[0]
+      if type_ is None:
+        raise ValueError("Unsupported datatype ", type(value[0]))
+      # we are dealing with an array
+      is_array = True
+
+      # int
+      if type_ == 0:
+        ints = [int(x) for x in value]
+        array_t = ctypes.c_long * len(ints)
+        array_ = array_t(*ints)
+      # float
+      elif type_ == 1:
+        floats = [float(x) for x in value]
+        array_t = ctypes.c_double * len(floats)
+        array_ = array_t(*floats)
+      # string
+      elif type_ == 2:
+        strings = [x.encode('utf-8') for x in value]
+        array_t = ctypes.c_double * len(ctypes.c_char_p)
+        array_ = array_t(*strings)
+      else:
+        raise ValueError("unkown type ", type)
+
+      # set size and cast array_ to void*
+      size_ = ctypes.c_size_t(len(value))
+      array_ = ctypes.cast(array_, ctypes.c_void_p)
+
+    # set type_ and call the super constructor
+    type_ = ctypes.c_uint(type_)
+
+    super().__init__(name_, type_, is_array, int_, double_, string_, array_,
+                     size_)
+
+  def __str__(self) -> str:
+    s = self.__repr__() + '\n'
+    for f in self._fields_:
+      attr_name = f[0]
+      attr = getattr(self, attr_name)
+      s += '  ' + attr_name + ': ' + str(attr) + '\n'
+    return s
+
+
 # load and destroy backend
 load_backend_func = tf_lib.aluminum_shark_loadBackend
 load_backend_func.argtypes = [ctypes.c_char_p]
@@ -82,6 +194,14 @@ create_backend_tfhe_func.argtypes = [
     ctypes.c_void_p  # backend handle
 ]
 create_backend_tfhe_func.restype = ctypes.c_void_p
+
+create_backend_ckks_dynamic_func = tf_lib.aluminum_shark_CreateContextCKKS_dynamic
+create_backend_ckks_dynamic_func.argtypes = [
+    ctypes.POINTER(aluminum_shark_Argument),  # array of arguments
+    ctypes.c_int,  # number of arguments
+    ctypes.c_void_p  # backend pointer
+]
+create_backend_ckks_dynamic_func.restype = ctypes.c_void_p
 
 ############################
 # context functions        #
@@ -610,6 +730,26 @@ class HEBackend(ObjectCleaner):
     super().destroy()
     destroy_backend_func(self.__handle)
     self.__handle = None
+
+  def createContext(self, scheme=None, **kwargs):
+    if scheme == 'ckks':
+      args = [
+          aluminum_shark_Argument(name=name, value=kwargs[name])
+          for name in kwargs
+      ]
+      AS_LOG('Creating Context. Arguments:')
+      for a in args:
+        print(a)
+      args_list = (ctypes.POINTER(aluminum_shark_Argument) *
+                   len(args))(*[ctypes.pointer(x) for x in args])
+
+      handle = create_backend_ckks_dynamic_func(
+          ctypes.cast(args_list, ctypes.POINTER(aluminum_shark_Argument)),
+          len(args), self.__handle)
+    else:
+      raise RuntimeError('not implemented yet')
+
+    return Context(handle, self)
 
   def createContextCKKS(self, poly_modulus_degree, coeff_modulus, scale):
     """
--- seal_backend/backend.h
+++ seal_backend/backend.h
@@ -28,6 +28,9 @@ class SEALBackend : public HEBackend {
                                        const std::vector<int>& coeff_modulus,
                                        double scale) override;
 
+  virtual HEContext* createContextCKKS(
+      std::vector<aluminum_shark_Argument> arguments) override;
+
   virtual const std::string& name() override;
   virtual const std::string& to_string() override;
   virtual const API_VERSION& api_version() override;
